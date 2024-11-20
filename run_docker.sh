015069f5cd3cf79adc5b52284bb79e0a0d97fe26
dogusyuksel <dogusyuk@gmail.com>
2024-11-20 10:09:42 +0000

    run script changed to run all the thing without being root (problem for yocto)

--- run_docker.sh
+++ run_docker.sh
@@ -16,7 +16,7 @@ if [ "$#" -gt 1 ]; then
         priv_string=$full_string
     done
     # echo "->$full_string<-"
-    docker run --rm -t --net=host -v $(pwd):/workspace --entrypoint=/bin/bash ${argv[0]} -c "$full_string"
+    docker run --rm -t --net=host -v $(pwd):/workspace -v /etc/passwd:/etc/passwd:ro -v /etc/group:/etc/group:ro -u $(id -u ${USER}):$(id -g ${USER}) --entrypoint=/bin/bash ${argv[0]} -c "$full_string"
 else
     docker run --rm -it --net=host -v $(pwd):/workspace -v /etc/passwd:/etc/passwd:ro -v /etc/group:/etc/group:ro -u $(id -u ${USER}):$(id -g ${USER}) --entrypoint=/bin/bash $@
 fi
